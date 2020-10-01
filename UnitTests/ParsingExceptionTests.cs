97bb4d34e4c2dec45a57924520b3cdcadab81fc8
Hotel2Oscar <waters.daniel.c@gmail.com>
2020-09-30 22:46:43 -0400

    Added ParsingException

---
+++ UnitTests/ParsingExceptionTests.cs
@@ -0,0 +1,17 @@
+﻿using Arcsecond;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class ParsingExceptionTests
+    {
+        [Test]
+        public void SimpleException()
+        {
+            var exception = new ParsingException("Illegal character 'a'", 15);
+
+            Assert.That(exception.Message, Is.EqualTo("Illegal character 'a' at index 15"));
+        }
+    }
+}
